7c7eb447499dcbb9b9d0a118d7d90487250ac1a7
ruslanIbr06 <rusibraev123@gmail.com>
2026-02-10 21:17:21 +0500

    Delete lab3/task1 directory

--- lab3/task1/chapter2.5.js
+++
@@ -1,7 +0,0 @@
-let name = "Ilya";
-
-alert( `hello ${1}` ); // Output: "hello 1" - ${1} replaces with number 1
-
-alert( `hello ${"name"}` ); // Output: "hello name" - ${"name"} is a string literal, not a variable
-
-alert( `hello ${name}` ); // Output: "hello Ilya" - ${name} replaces with value of variable name
